46facd8e7d2e6fda3756053364464ba7176d78ca
Astha Saxena <astha.sxn31@gmail.com>
2016-09-22 23:41:23 -0500

    delegate and advisor models

--- muni/advisornetwork/models.py
+++ muni/advisornetwork/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+class Delegate(models.Model):
+    name = models.CharField(max_length=500)
+    position = models.CharField(max_length=500)
+    committee = models.CharField(max_length=500)
+    hotel_room_number = models.CharField(max_length=500)
+
+class Advisor(models.Model):
+    name = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    work_phone_number = models.CharField(max_length=20)
+    mobile_phone_number = models.CharField(max_length=20)
+
+
